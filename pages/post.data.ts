fd2c95181f9245dc39988760bf35d23abfea2f13
hsiaosiyuan0 <hsiaosiyuan0@outlook.com>
2020-12-27 18:36:15 +0800

    fix: test docs

--- pages/post.data.ts
+++ pages/post.data.ts
@@ -5,8 +5,8 @@ import unified from "unified";
 import markdown from "remark-parse";
 import unist from "unist";
 import vfile from "vfile";
-import { catalogFilename } from "gadget.js/ignition";
 
+export const catalogFilename = "_catalog.md";
 export const WORDS = process.env.WORDS ?? process.cwd();
 export const BASENAME = process.env.PUBLIC_URL ?? "";
 
